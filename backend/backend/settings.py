13f1d71b551ea1ab9e1c788e2417a6b0bf2e4273
69sarthak69 <127380488+69sarthak69@users.noreply.github.com>
2025-06-07 13:18:11 +0545

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -183,8 +183,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shresthasarthak666@gmail.com'  
+EMAIL_HOST_USER = ''  
 EMAIL_HOST_PASSWORD = 'gqlpmldtqefmperd' 
-DEFAULT_FROM_EMAIL = 'CommunityHelp <shresthasarthak666@gmail.com>'
+DEFAULT_FROM_EMAIL = 'CommunityHelp <>'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
